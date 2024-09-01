69d512e989a30b3dc4ea37e672fc06ca1c394a90
SSR-Med <sasalazarr@unal.edu.co>
2024-09-01 18:28:51 -0500

    Fix(ItemsxUser): GetQuantityItems4User now returns a json instead of a list, it is easier for fetching data

--- src/services/item/ItemxUserService.ts
+++ src/services/item/ItemxUserService.ts
@@ -1,24 +1,37 @@
 // Dependencies
 import { fn } from "sequelize";
+import Sequelize from "sequelize";
 // Models
 import { ItemxUser } from "../../models/ItemxUser";
+import { Item } from "../../models/Item";
 // Custom error
 import { httpError } from "../../config/CustomError";
 
 export async function getQuantityItem4User(userId: number) {
     const items = await ItemxUser.findAll({
         where: { id_user: userId },
-        group: ['id_item'],
-        attributes: ['id_item', [fn('COUNT', 'id_item'), 'quantity']]
+        group: ['id_item', 'item.name'],
+        attributes: ['id_item', [fn('COUNT', 'id_item'), 'quantity'],
+            [Sequelize.col('item.name'), 'item_name']
+        ],
+        include: [
+            {
+                model: Item,
+                attributes: [],
+                required: true
+            }
+        ]
     });
 
     // Create a new object to store the desired JSON structure
-    const itemQuantityMap: { [key: string]: number } = {};
+    const itemQuantityMap: { [key: string]: any } = {};
 
     // Iterate over the items and populate the map
-    items.forEach((item: { dataValues: {
-        quantity: number; id_item: number }}) => {
-        itemQuantityMap[item.dataValues.id_item] = Number(item.dataValues.quantity);
+    items.forEach((item: Record<string,any>) => {
+        itemQuantityMap[item.dataValues.item_name] = {
+            quantity: Number(item.dataValues.quantity),
+            id: Number(item.dataValues.id_item)
+        }
     });
 
     // Return the JSON object
