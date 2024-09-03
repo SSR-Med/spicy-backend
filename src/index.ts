6b2db82ca26b5bd698b3372f435360b8c7af7deb
LostOni <maimorenovaloyes@gmail.com>
2024-09-02 21:55:21 -0500

    Añadidos World y Mission

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,8 @@ import {User} from "./models/User"
 import {Card} from "./models/Card";
 import { Item } from './models/Item';
 import { ItemxUser } from './models/ItemxUser';
+import { World } from './models/World';
+import { Mission } from './models/Mission';
 require("./models/Associations");
 
 // Routes
@@ -26,12 +28,13 @@ app.use("/api/admin",require("./routes/user/Admin"));
 app.use("/api/card",require("./routes/card/Card"));
 app.use("/api/item",require("./routes/item/Item"));
 app.use("/api/user/item", require("./routes/item/ItemxUser"));
+app.use("/api/world", require("./routes/world/WorldRoute"));
 
 // Server running
 database
   .authenticate()
   .then(() => {
-    return database.sync();
+    return database.sync({alter: true});
   })
   .then(() => {
     app.listen(port, () => {
--- src/models/Associations.ts
+++ src/models/Associations.ts
@@ -2,6 +2,8 @@
 import {User} from "./User";
 import { Item } from "./Item";
 import { ItemxUser } from "./ItemxUser";
+import { World } from "./World";
+import { Mission } from "./Mission";
 
 // Associations
 
@@ -9,3 +11,5 @@ User.hasMany(ItemxUser, {onDelete: 'cascade',foreignKey: 'id_user'});
 ItemxUser.belongsTo(User, {foreignKey: 'id_user'});
 Item.hasMany(ItemxUser, {onDelete:'cascade',foreignKey: 'id_item'});
 ItemxUser.belongsTo(Item, {foreignKey: 'id_item'});
+World.hasMany(Mission, {onDelete: 'cascade', foreignKey: 'worldId'});
+Mission.belongsTo(World, {foreignKey: 'worldId'});
